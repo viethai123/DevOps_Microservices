3b3576e9f86eafd08d145bd6af4394b473538930
haringuyen <viethai180199@gmail.com>
2023-05-30 19:14:08 +0000

    update code

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,14 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath="viethainguyentran/microproject:v1.0.0"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag microproject ${dockerpath}
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}
